ae6113fc2c6fd8fea942e0651b847a7448398c3b
Håkon <haakonu97@gmail.com>
2024-06-22 02:50:38 +0200

    chore: fixed build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,33 +1,8 @@
-import React from "react";
-export declare function NativePopover<T extends HTMLElement = HTMLElement>(props: {
-    trigger: (props: {
-        popovertarget: string;
-        id: string;
-    }) => React.ReactNode;
-    popover: (props: {
-        id: string;
-        popover: string;
-        ref?: (element: T | null) => void;
-        anchor?: string;
-    }, closeProps: {
-        popovertarget: string;
-        popovertargetaction: string;
-    }) => React.ReactNode;
-    type?: "auto" | "manual";
-    id?: string;
-    control?: (element: T | null) => void;
-    anchor?: boolean;
-}): React.JSX.Element;
-export declare function usePopoverControls<T extends HTMLElement = HTMLElement>(args?: {
-    throwOnError?: boolean;
-    onError?: (error: string) => void;
-}): {
-    show: () => void;
-    hide: () => void;
-    toggle: () => boolean | void;
-    control: (element: T | null) => void;
-    isOpen: boolean;
-    id: string | null;
-};
-export declare function registerPopoverListener(element: HTMLElement | undefined | null, callback: (event: Event) => void): () => void;
-export declare function browserSupportsPopover(): boolean;
+import React, { ReactNode } from "react";
+type Component<T> = (props: T) => ReactNode;
+export declare const createScoped: <T extends Component<P> | keyof React.JSX.IntrinsicElements = "div", P = T extends keyof React.JSX.IntrinsicElements ? React.ComponentProps<T> : T extends (...args: any[]) => any ? Parameters<T>[0] : never>(element: T) => (strings: TemplateStringsArray, ...args: ((props: P) => string)[]) => (props: P) => React.JSX.Element;
+declare const Component: (props: {
+    children: ReactNode;
+    color: string;
+}) => React.JSX.Element;
+export {};
--- dist/index.js
+++ dist/index.js
@@ -1,146 +1,30 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.browserSupportsPopover = exports.registerPopoverListener = exports.usePopoverControls = exports.NativePopover = void 0;
-const react_1 = __importStar(require("react"));
-function NativePopover(props) {
-    var _a;
-    const anchorId = (0, react_1.useRef)(generateId());
-    const id = (0, react_1.useRef)(generateId());
-    if (props.id) {
-        id.current = props.id;
-    }
-    return (react_1.default.createElement(react_1.default.Fragment, null,
-        props.trigger({
-            popovertarget: id.current,
-            id: anchorId.current,
-        }),
-        props.popover({
-            id: id.current,
-            popover: (_a = props.type) !== null && _a !== void 0 ? _a : "auto",
-            ref: props.control,
-            anchor: props.anchor ? anchorId.current : undefined,
-        }, {
-            popovertarget: id.current,
-            popovertargetaction: "hide",
-        })));
-}
-exports.NativePopover = NativePopover;
-const errors = {
-    notSupported: "Popover API is not supported in your browser",
-    notMounted: "Target element is not mounted, or does not support popover API",
+exports.createScoped = void 0;
+const react_1 = __importDefault(require("react"));
+const createScoped = (element) => {
+    const scoped = (strings, ...args) => function Component(props) {
+        const resolved = args.map((arg) => arg(props));
+        const styles = strings.reduce((acc, string, i) => { var _a; return acc + string + ((_a = resolved[i]) !== null && _a !== void 0 ? _a : ""); }, "");
+        const Element = element;
+        return (
+        // @ts-ignore
+        react_1.default.createElement(Element, Object.assign({}, props),
+            react_1.default.createElement("style", null, `@scope { ${styles} }`),
+            props.children));
+    };
+    return scoped;
 };
-function usePopoverControls(args) {
-    const hasSetInitial = (0, react_1.useRef)(false);
-    const registeredId = (0, react_1.useRef)(null);
-    const ref = (0, react_1.useRef)(null);
-    const listenerRegistered = (0, react_1.useRef)(false);
-    const [isOpen, setIsOpen] = (0, react_1.useState)(false);
-    const createError = (error) => {
-        if (args === null || args === void 0 ? void 0 : args.onError) {
-            args.onError(error);
-        }
-        if (args === null || args === void 0 ? void 0 : args.throwOnError) {
-            throw new Error(error);
-        }
-        else {
-            console.error(errors);
-        }
-    };
-    const show = (0, react_1.useCallback)(() => {
-        if (!browserSupportsPopover()) {
-            return createError(errors.notSupported);
-        }
-        if (!ref.current || !("showPopover" in ref.current))
-            return createError(errors.notMounted);
-        return ref.current.showPopover();
-    }, []);
-    const hide = (0, react_1.useCallback)(() => {
-        if (!browserSupportsPopover()) {
-            return createError(errors.notSupported);
-        }
-        if (!ref.current || !("togglePopover" in ref.current))
-            return createError(errors.notMounted);
-        return ref.current.hidePopover();
-    }, []);
-    const toggle = (0, react_1.useCallback)(() => {
-        if (!browserSupportsPopover()) {
-            return createError(errors.notSupported);
-        }
-        if (!ref.current || !("togglePopover" in ref.current))
-            return createError(errors.notMounted);
-        return ref.current.togglePopover();
-    }, []);
-    const beforeToggle = (0, react_1.useCallback)((event) => {
-        if ("newState" in event && event.newState === "open") {
-            setIsOpen(true);
-        }
-        if ("newState" in event && event.newState === "closed") {
-            setIsOpen(false);
-        }
-    }, [setIsOpen]);
-    const refApplier = (0, react_1.useCallback)((element) => {
-        if (element) {
-            if (!(element instanceof HTMLElement)) {
-                return createError("Element passed to ref is not an HTMLElement");
-            }
-            ref.current = element;
-            registeredId.current = element.id;
-            listenerRegistered.current = true;
-            element.addEventListener("beforetoggle", beforeToggle);
-            if (hasSetInitial.current) {
-                setIsOpen(ref.current.matches(":popover-open"));
-            }
-        }
-    }, []);
-    (0, react_1.useEffect)(registerPopoverListener(ref.current, beforeToggle), [
-        ref.current,
-    ]);
-    return {
-        show,
-        hide,
-        toggle,
-        control: refApplier,
-        isOpen,
-        id: registeredId.current,
-    };
-}
-exports.usePopoverControls = usePopoverControls;
-function registerPopoverListener(element, callback) {
-    element === null || element === void 0 ? void 0 : element.addEventListener("beforetoggle", callback);
-    return () => {
-        element === null || element === void 0 ? void 0 : element.removeEventListener("beforetoggle", callback);
-    };
-}
-exports.registerPopoverListener = registerPopoverListener;
-function browserSupportsPopover() {
-    // eslint-disable-next-line no-prototype-builtins
-    return HTMLElement.prototype.hasOwnProperty("popover");
-}
-exports.browserSupportsPopover = browserSupportsPopover;
-let i = 0;
-function generateId() {
-    return `popover-${i++}`;
-}
+exports.createScoped = createScoped;
+const TestA = (0, exports.createScoped)("div") `
+  color: ${(props) => { var _a, _b; return (_b = (_a = props["aria-busy"]) === null || _a === void 0 ? void 0 : _a.toString()) !== null && _b !== void 0 ? _b : ""; }};
+`;
+const Component = (props) => {
+    return react_1.default.createElement(TestA, Object.assign({}, props));
+};
+const TestB = (0, exports.createScoped)(Component) `
+  color: ${(props) => props.color};
+`;
